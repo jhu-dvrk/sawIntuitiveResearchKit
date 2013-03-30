7442c2079141eb0c265fbe1287c4f104cf2af468
Anton Deguet <anton.deguet@jhu.edu>
2013-03-30 02:22:11 +0000

    /trunk, sawIntuitiveResearchKit: work in progress, added .rob files provided by S. Leonard, added shell for tele-operation, forward kinematic on master seems to work
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk/saw/applications/sawIntuitiveResearchKit@4042 0959eb1a-6956-4f60-947b-6092a4e22c97

---
+++ mainQtTeleOperation.cpp
@@ -0,0 +1,153 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+/*
+  $Id$
+
+  Author(s):  Zihan Chen
+  Created on: 2013-02-07
+
+  (C) Copyright 2013 Johns Hopkins University (JHU), All Rights Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+
+*/
+
+// system
+#include <iostream>
+// cisst/saw
+#include <cisstCommon/cmnPath.h>
+#include <cisstCommon/cmnCommandLineOptions.h>
+#include <sawRobotIO1394/mtsRobotIO1394.h>
+#include <sawRobotIO1394/mtsRobotIO1394QtWidget.h>
+#include <sawControllers/mtsPID.h>
+#include <sawControllers/mtsPIDQtWidget.h>
+#include <sawControllers/mtsTeleOperation.h>
+#include <sawControllers/mtsTeleOperationQtWidget.h>
+
+int main(int argc, char ** argv)
+{
+    // log configuration
+    cmnLogger::SetMask(CMN_LOG_ALLOW_ALL);
+    cmnLogger::SetMaskDefaultLog(CMN_LOG_ALLOW_ALL);
+    cmnLogger::AddChannel(std::cerr, CMN_LOG_ALLOW_ERRORS_AND_WARNINGS);
+
+    // create a Qt application
+    QApplication application(argc, argv);
+
+    // parse options
+    cmnCommandLineOptions options;
+    int firewirePort;
+    std::string ioConfigFile, pidConfigFile, kinConfigFile;
+    options.AddOptionOneValue("i", "io",
+                              "configuration file for robot IO (see sawRobotIO1394)",
+                              cmnCommandLineOptions::REQUIRED, &ioConfigFile);
+    options.AddOptionOneValue("p", "pid",
+                              "configuration file for PID controller (see sawControllers, mtsPID)",
+                              cmnCommandLineOptions::REQUIRED, &pidConfigFile);
+    options.AddOptionOneValue("k", "kinematic",
+                              "configuration file for kinematic (see cisstRobot, robManipulator)",
+                              cmnCommandLineOptions::REQUIRED, &kinConfigFile);
+    options.AddOptionOneValue("f", "firewire",
+                              "firewire port number(s)",
+                              cmnCommandLineOptions::REQUIRED, &firewirePort);
+    std::string errorMessage;
+    if (!options.Parse(argc, argv, errorMessage)) {
+        std::cerr << "Error: " << errorMessage << std::endl;
+        options.PrintUsage(std::cerr);
+        return -1;
+    }
+
+    if (!cmnPath::Exists(ioConfigFile)) {
+        std::cerr << "File not found: " << ioConfigFile << std::endl;
+        return -1;
+    }
+    if (!cmnPath::Exists(pidConfigFile)) {
+        std::cerr << "File not found: " << pidConfigFile << std::endl;
+        return -1;
+    }
+    if (!cmnPath::Exists(kinConfigFile)) {
+        std::cerr << "File not found: " << kinConfigFile << std::endl;
+        return -1;
+    }
+    std::cout << "Configuration file for IO: " << ioConfigFile << std::endl
+              << "Configuration file for PID: " << pidConfigFile << std::endl
+              << "Configuration file for kin: " << kinConfigFile << std::endl
+              << "FirewirePort: " << firewirePort << std::endl;
+
+    mtsManagerLocal * manager = mtsManagerLocal::GetInstance();
+
+    // IO
+    mtsRobotIO1394QtWidget * ioGUI = new mtsRobotIO1394QtWidget("ioGUI");
+    ioGUI->Configure();
+    manager->AddComponent(ioGUI);
+    mtsRobotIO1394 * io = new mtsRobotIO1394("io", 1.0 * cmn_ms, firewirePort, ioGUI->GetOutputStream());
+    io->Configure(ioConfigFile);
+    manager->AddComponent(io);
+    // connect ioGUI to io
+    manager->Connect("ioGUI", "Robot", "io", "MTML");
+    manager->Connect("ioGUI", "RobotActuators", "io", "MTMLActuators");
+
+    // PID
+    mtsPIDQtWidget * pidGUI = new mtsPIDQtWidget("pidGUI");
+    pidGUI->Configure();
+    manager->AddComponent(pidGUI);
+    mtsPID * pid = new mtsPID("pid", 1 * cmn_ms);
+    pid->Configure(pidConfigFile);
+    manager->AddComponent(pid);
+    // connect pidGUI to pid
+    manager->Connect("pidGUI", "Controller", "pid", "Controller");
+
+    // Teleoperation
+    mtsTeleOperationQtWidget * teleGUI = new mtsTeleOperationQtWidget("teleGUI");
+    teleGUI->Configure();
+    manager->AddComponent(teleGUI);
+    mtsTeleOperation * tele = new mtsTeleOperation("tele", 1.0 * cmn_ms);
+    tele->ConfigureMaster(kinConfigFile);
+    manager->AddComponent(tele);
+    // connect teleGUI to tele
+    manager->Connect("teleGUI", "TeleOperation", "tele", "Setting");
+
+    // connect interfaces
+    manager->Connect("pid", "RobotJointTorqueInterface", "io", "MTML");
+    manager->Connect("tele", "Master", "io", "MTML");
+
+    // execute in following order usign a single thread
+    manager->Connect("pid", "ExecIn", "io", "ExecOut");
+    manager->Connect("tele", "ExecIn", "pid", "ExecOut");
+
+    //-------------- create the components ------------------
+    manager->CreateAll();
+    manager->WaitForStateAll(mtsComponentState::READY, 2.0 * cmn_s);
+
+    // start the periodic Run
+    manager->StartAll();
+
+    // create a main window to hold QWidget
+    ioGUI->show();
+    pidGUI->show();
+    teleGUI->show();
+
+    // run Qt app
+    application.exec();
+
+    manager->KillAll();
+    manager->WaitForStateAll(mtsComponentState::FINISHED, 2.0 * cmn_s);
+    manager->Cleanup();
+
+    // delete dvgc robot
+    delete pid;
+    delete pidGUI;
+    delete io;
+    delete ioGUI;
+
+    // stop all logs
+    cmnLogger::Kill();
+
+    return 0;
+}
