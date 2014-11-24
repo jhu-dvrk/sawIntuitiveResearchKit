a5fad866643d2611a31b2c017ad532032d406ae6
Anton Deguet <anton.deguet@jhu.edu>
2014-11-24 15:53:20 -0500

    Updated some examples to filter logs using classes matching mtsIntuitiveReseachKit

--- examples/mainQtArm.cpp
+++ examples/mainQtArm.cpp
@@ -41,7 +41,7 @@ int main(int argc, char ** argv)
     cmnLogger::SetMask(CMN_LOG_ALLOW_ALL);
     cmnLogger::SetMaskDefaultLog(CMN_LOG_ALLOW_ALL);
     cmnLogger::SetMaskFunction(CMN_LOG_ALLOW_ALL);
-    cmnLogger::SetMaskClass("mtsIntuitiveResearchKitPSM", CMN_LOG_ALLOW_ALL);
+    cmnLogger::SetMaskClassMatching("mtsIntuitiveResearchKit", CMN_LOG_ALLOW_ALL);
     cmnLogger::AddChannel(std::cerr, CMN_LOG_ALLOW_ERRORS_AND_WARNINGS);
 
     // parse options
