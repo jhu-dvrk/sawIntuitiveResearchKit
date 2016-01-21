f8d94ddeb3dd5a90d181f04d300114d1a6f21180
Anton Deguet <anton.deguet@jhu.edu>
2016-01-21 16:58:13 -0500

    ECM teleop, added empty shell for bimanual teleop component

---
+++ code/mtsTeleOperationBimanual.cpp
@@ -0,0 +1,416 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+
+/*
+  Author(s):  Anton Deguet
+  Created on: 2016-01-21
+
+  (C) Copyright 2016 Johns Hopkins University (JHU), All Rights Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+*/
+
+
+// system include
+#include <iostream>
+
+// cisst
+#include <sawIntuitiveResearchKit/mtsTeleOperationBimanual.h>
+#include <cisstMultiTask/mtsInterfaceProvided.h>
+#include <cisstMultiTask/mtsInterfaceRequired.h>
+
+
+CMN_IMPLEMENT_SERVICES_DERIVED_ONEARG(mtsTeleOperationBimanual, mtsTaskPeriodic, mtsTaskPeriodicConstructorArg);
+
+mtsTeleOperationBimanual::mtsTeleOperationBimanual(const std::string & componentName, const double periodInSeconds):
+    mtsTaskPeriodic(componentName, periodInSeconds)
+{
+    Init();
+}
+
+mtsTeleOperationBimanual::mtsTeleOperationBimanual(const mtsTaskPeriodicConstructorArg & arg):
+    mtsTaskPeriodic(arg)
+{
+    Init();
+}
+
+void mtsTeleOperationBimanual::Init(void)
+{
+    mScale = 0.2;
+
+    // Initialize states
+    mIsClutched = false;
+    mIsOperatorPresent = false;
+    mIsEnabled = false;
+    mSlave.IsManipClutched = false;
+
+    StateTable.AddData(mMasterLeft.PositionCartesianCurrent, "MasterLeftCartesianPosition");
+    StateTable.AddData(mMasterRight.PositionCartesianCurrent, "MasterRightCartesianPosition");
+    StateTable.AddData(mSlave.PositionCartesianCurrent, "SlaveCartesianPosition");
+
+    mConfigurationStateTable = new mtsStateTable(100, "Configuration");
+    mConfigurationStateTable->SetAutomaticAdvance(false);
+    AddStateTable(mConfigurationStateTable);
+    mConfigurationStateTable->AddData(mScale, "Scale");
+    mConfigurationStateTable->AddData(mRegistrationRotation, "RegistrationRotation");
+
+    mtsInterfaceRequired * interfaceRequired = AddInterfaceRequired("MasterLeft");
+    if (interfaceRequired) {
+        interfaceRequired->AddFunction("GetPositionCartesian", mMasterLeft.GetPositionCartesian);
+        interfaceRequired->AddFunction("SetRobotControlState", mMasterLeft.SetRobotControlState);
+        interfaceRequired->AddEventHandlerWrite(&mtsTeleOperationBimanual::MasterLeftErrorEventHandler,
+                                                this, "Error");
+    }
+
+    interfaceRequired = AddInterfaceRequired("MasterRight");
+    if (interfaceRequired) {
+        interfaceRequired->AddFunction("GetPositionCartesian", mMasterRight.GetPositionCartesian);
+        interfaceRequired->AddFunction("SetRobotControlState", mMasterRight.SetRobotControlState);
+        interfaceRequired->AddEventHandlerWrite(&mtsTeleOperationBimanual::MasterRightErrorEventHandler,
+                                                this, "Error");
+    }
+
+    interfaceRequired = AddInterfaceRequired("Slave");
+    if (interfaceRequired) {
+        interfaceRequired->AddFunction("GetPositionCartesian", mSlave.GetPositionCartesian);
+        interfaceRequired->AddFunction("SetPositionCartesian", mSlave.SetPositionCartesian);
+        interfaceRequired->AddFunction("SetRobotControlState", mSlave.SetRobotControlState);
+        interfaceRequired->AddEventHandlerWrite(&mtsTeleOperationBimanual::SlaveErrorEventHandler,
+                                                this, "Error");
+        interfaceRequired->AddEventHandlerWrite(&mtsTeleOperationBimanual::SlaveClutchEventHandler,
+                                                this, "ManipClutch");
+    }
+
+    // Footpedal events
+    interfaceRequired = AddInterfaceRequired("Clutch");
+    if (interfaceRequired) {
+        interfaceRequired->AddEventHandlerWrite(&mtsTeleOperationBimanual::ClutchEventHandler,
+                                                this, "Button");
+    }
+
+    interfaceRequired = AddInterfaceRequired("OperatorPresent");
+    if (interfaceRequired) {
+        interfaceRequired->AddEventHandlerWrite(&mtsTeleOperationBimanual::OperatorPresentEventHandler,
+                                                this, "Button");
+    }
+
+    mtsInterfaceProvided * interfaceProvided = AddInterfaceProvided("Setting");
+    if (interfaceProvided) {
+        // commands
+        interfaceProvided->AddCommandReadState(StateTable, StateTable.PeriodStats,
+                                              "GetPeriodStatistics"); // mtsIntervalStatistics
+
+        interfaceProvided->AddCommandWrite(&mtsTeleOperationBimanual::Enable, this,
+                                           "Enable", false);
+        interfaceProvided->AddCommandWrite(&mtsTeleOperationBimanual::SetScale, this,
+                                           "SetScale", 0.5);
+        interfaceProvided->AddCommandWrite(&mtsTeleOperationBimanual::SetRegistrationRotation, this,
+                                           "SetRegistrationRotation", vctMatRot3());
+        interfaceProvided->AddCommandReadState(*mConfigurationStateTable,
+                                               mScale,
+                                               "GetScale");
+        interfaceProvided->AddCommandReadState(*mConfigurationStateTable,
+                                               mRegistrationRotation,
+                                               "GetRegistrationRotation");
+        interfaceProvided->AddCommandReadState(StateTable,
+                                               mMasterLeft.PositionCartesianCurrent,
+                                               "GetPositionCartesianMasterLeft");
+        interfaceProvided->AddCommandReadState(StateTable,
+                                               mMasterRight.PositionCartesianCurrent,
+                                               "GetPositionCartesianMasterRight");
+        interfaceProvided->AddCommandReadState(StateTable,
+                                               mSlave.PositionCartesianCurrent,
+                                               "GetPositionCartesianSlave");
+        // events
+        interfaceProvided->AddEventWrite(MessageEvents.Status,
+                                         "Status", std::string(""));
+        interfaceProvided->AddEventWrite(MessageEvents.Warning,
+                                         "Warning", std::string(""));
+        interfaceProvided->AddEventWrite(MessageEvents.Error,
+                                         "Error", std::string(""));
+        interfaceProvided->AddEventWrite(MessageEvents.Enabled,
+                                         "Enabled", false);
+        // configuration
+        interfaceProvided->AddEventWrite(ConfigurationEvents.Scale,
+                                         "Scale", 0.5);
+    }
+}
+
+void mtsTeleOperationBimanual::Configure(const std::string & filename)
+{
+    CMN_LOG_CLASS_INIT_VERBOSE << "Configure: " << filename << std::endl;
+}
+
+void mtsTeleOperationBimanual::Startup(void)
+{
+    CMN_LOG_CLASS_INIT_VERBOSE << "Startup" << std::endl;
+}
+
+void mtsTeleOperationBimanual::Run(void)
+{
+    ProcessQueuedCommands();
+    ProcessQueuedEvents();
+
+#if 0
+    // get master Cartesian position
+    mtsExecutionResult executionResult;
+    executionResult = Master.GetPositionCartesian(Master.PositionCartesianCurrent);
+    if (!executionResult.IsOK()) {
+        CMN_LOG_CLASS_RUN_ERROR << "Run: call to Master.GetPositionCartesian failed \""
+                                << executionResult << "\"" << std::endl;
+        MessageEvents.Error(this->GetName() + ": unable to get cartesian position from master");
+        this->Enable(false);
+    }
+    vctFrm4x4 masterPosition(Master.PositionCartesianCurrent.Position());
+
+    // get slave Cartesian position
+    executionResult = Slave.GetPositionCartesian(Slave.PositionCartesianCurrent);
+    if (!executionResult.IsOK()) {
+        CMN_LOG_CLASS_RUN_ERROR << "Run: call to Slave.GetPositionCartesian failed \""
+                                << executionResult << "\"" << std::endl;
+        MessageEvents.Error(this->GetName() + ": unable to get cartesian position from slave");
+        this->Enable(false);
+    }
+
+    /*!
+      mtsTeleOperationBimanual can run in 4 control modes, which is controlled by
+      footpedal Clutch & OperatorPresent.
+
+      Mode 1: OperatorPresent = False, Clutch = False
+              MTM and PSM stop at their current position. If PSM ManipClutch is
+              pressed, then the user can manually move PSM.
+              NOTE: MTM always tries to allign its orientation with PSM's orientation
+
+      Mode 2/3: OperatorPresent = False/True, Clutch = True
+              MTM can move freely in workspace, however its orientation is locked
+              PSM can not move
+
+      Mode 4: OperatorPresent = True, Clutch = False
+              PSM follows MTM motion
+    */
+    if (IsEnabled
+        && Master.PositionCartesianCurrent.Valid()
+        && Slave.PositionCartesianCurrent.Valid()) {
+        // follow mode
+        if (!IsClutched && IsOperatorPresent) {
+            // compute master Cartesian motion
+            vctFrm4x4 masterCartesianMotion;
+            masterCartesianMotion = Master.CartesianPrevious.Inverse() * masterPosition;
+
+            // translation
+            vct3 masterTranslation;
+            vct3 slaveTranslation;
+            if (this->TranslationLocked) {
+                slaveTranslation = Slave.CartesianPrevious.Translation();
+            } else {
+                masterTranslation = (masterPosition.Translation() - Master.CartesianPrevious.Translation());
+                slaveTranslation = masterTranslation * this->Scale;
+                slaveTranslation = RegistrationRotation * slaveTranslation + Slave.CartesianPrevious.Translation();
+            }
+            // rotation
+            vctMatRot3 slaveRotation;
+            if (this->RotationLocked) {
+                slaveRotation.From(Slave.CartesianPrevious.Rotation());
+            } else {
+                slaveRotation = RegistrationRotation * masterPosition.Rotation();
+            }
+
+            // compute desired slave position
+            vctFrm4x4 slaveCartesianDesired;
+            slaveCartesianDesired.Translation().Assign(slaveTranslation);
+            slaveCartesianDesired.Rotation().FromNormalized(slaveRotation);
+            Slave.PositionCartesianDesired.Goal().FromNormalized(slaveCartesianDesired);
+
+            // Slave go this cartesian position
+            Slave.SetPositionCartesian(Slave.PositionCartesianDesired);
+
+            // Gripper
+            if (Master.GetGripperPosition.IsValid()) {
+                double gripperPosition;
+                Master.GetGripperPosition(gripperPosition);
+                Slave.SetJawPosition(gripperPosition);
+            } else {
+                Slave.SetJawPosition(5.0 * cmnPI_180);
+            }
+        } else if (!IsClutched && !IsOperatorPresent) {
+            // Do nothing
+        }
+    } else {
+        CMN_LOG_CLASS_RUN_DEBUG << "mtsTeleOperationBimanual disabled" << std::endl;
+    }
+#endif
+}
+
+void mtsTeleOperationBimanual::Cleanup(void)
+{
+    CMN_LOG_CLASS_INIT_VERBOSE << "Cleanup" << std::endl;
+}
+
+void mtsTeleOperationBimanual::MasterLeftErrorEventHandler(const std::string & message)
+{
+    Enable(false);
+    MessageEvents.Error(this->GetName() + ": received from left master [" + message + "]");
+}
+
+void mtsTeleOperationBimanual::MasterRightErrorEventHandler(const std::string & message)
+{
+    Enable(false);
+    MessageEvents.Error(this->GetName() + ": received from right master [" + message + "]");
+}
+
+void mtsTeleOperationBimanual::SlaveErrorEventHandler(const std::string & message)
+{
+    Enable(false);
+    MessageEvents.Error(this->GetName() + ": received from slave [" + message + "]");
+}
+
+void mtsTeleOperationBimanual::SlaveClutchEventHandler(const prmEventButton & button)
+{
+    if (button.Type() == prmEventButton::PRESSED) {
+        mSlave.IsManipClutched = true;
+        MessageEvents.Status(this->GetName() + ": slave clutch pressed");
+    } else {
+        mSlave.IsManipClutched = false;
+        MessageEvents.Status(this->GetName() + ": slave clutch released");
+    }
+
+#if 0
+    // Slave State
+    if (IsEnabled && !IsOperatorPresent && Slave.IsManipClutched) {
+        Slave.SetRobotControlState(mtsStdString("Manual"));
+    } else if (IsEnabled) {
+        Slave.SetRobotControlState(mtsStdString("Teleop"));
+    }
+#endif
+}
+
+void mtsTeleOperationBimanual::CameraClutchEventHandler(const prmEventButton & button)
+{
+    if (button.Type() == prmEventButton::PRESSED) {
+        mSlave.IsManipClutched = true;
+        MessageEvents.Status(this->GetName() + ": camera clutch pressed");
+    } else {
+        mSlave.IsManipClutched = false;
+        MessageEvents.Status(this->GetName() + ": camera clutch released");
+    }
+}
+
+void mtsTeleOperationBimanual::ClutchEventHandler(const prmEventButton & button)
+{
+#if 0
+    mtsExecutionResult executionResult;
+    executionResult = Master.GetPositionCartesian(Master.PositionCartesianCurrent);
+    if (!executionResult.IsOK()) {
+        CMN_LOG_CLASS_RUN_ERROR << "EventHandlerClutched: call to Master.GetPositionCartesian failed \""
+                                << executionResult << "\"" << std::endl;
+    }
+    executionResult = Slave.GetPositionCartesian(Slave.PositionCartesianCurrent);
+    if (!executionResult.IsOK()) {
+        CMN_LOG_CLASS_RUN_ERROR << "EventHandlerClutched: call to Slave.GetPositionCartesian failed \""
+                                << executionResult << "\"" << std::endl;
+    }
+
+    if (button.Type() == prmEventButton::PRESSED) {
+        this->IsClutched = true;
+        Master.PositionCartesianDesired.Goal().Rotation().FromNormalized(
+                    Slave.PositionCartesianCurrent.Position().Rotation());
+        Master.PositionCartesianDesired.Goal().Translation().Assign(
+                    Master.PositionCartesianCurrent.Position().Translation());
+        MessageEvents.Status(this->GetName() + ": master clutch pressed");
+    } else {
+        this->IsClutched = false;
+        MessageEvents.Status(this->GetName() + ": master clutch released");
+    }
+    SetMasterControlState();
+#endif
+}
+
+void mtsTeleOperationBimanual::OperatorPresentEventHandler(const prmEventButton & button)
+{
+    if (button.Type() == prmEventButton::PRESSED) {
+        mIsOperatorPresent = true;
+        MessageEvents.Status(this->GetName() + ": operator present");
+    } else {
+        mIsOperatorPresent = false;
+        MessageEvents.Status(this->GetName() + ": operator not present");
+    }
+#if 0
+    SetMasterControlState();
+#endif
+}
+
+void mtsTeleOperationBimanual::Enable(const bool & enable)
+{
+    mIsEnabled = enable;
+
+#if 0
+    if (mIsEnabled) {
+        // Set Master/Slave to Teleop (Cartesian Position Mode)
+        SetMasterControlState();
+        Slave.SetRobotControlState(mtsStdString("Teleop"));
+
+        // Orientate Master with Slave
+        vctFrm4x4 masterCartesianDesired;
+        masterCartesianDesired.Translation().Assign(MasterLockTranslation);
+        vctMatRot3 masterRotation;
+        masterRotation = RegistrationRotation.Inverse() * Slave.PositionCartesianCurrent.Position().Rotation();
+        masterCartesianDesired.Rotation().FromNormalized(masterRotation);
+
+        // Send Master command postion
+        Master.SetRobotControlState(mtsStdString("DVRK_POSITION_GOAL_CARTESIAN"));
+        Master.PositionCartesianDesired.Goal().FromNormalized(masterCartesianDesired);
+        Master.SetPositionGoalCartesian(Master.PositionCartesianDesired);
+    }
+
+    // Send event for GUI
+#endif
+    MessageEvents.Enabled(mIsEnabled);
+}
+
+void mtsTeleOperationBimanual::SetScale(const double & scale)
+{
+    mConfigurationStateTable->Start();
+    mScale = scale;
+    mConfigurationStateTable->Advance();
+    ConfigurationEvents.Scale(mScale);
+}
+
+void mtsTeleOperationBimanual::SetRegistrationRotation(const vctMatRot3 & rotation)
+{
+    mConfigurationStateTable->Start();
+    mRegistrationRotation = rotation;
+    mConfigurationStateTable->Advance();
+}
+
+#if 0
+void mtsTeleOperationBimanual::SetMasterControlState(void)
+{
+    if (mIsEnabled == false) {
+        CMN_LOG_CLASS_RUN_WARNING << "TeleOperation is NOT enabled" << std::endl;
+        return;
+    }
+
+    if (IsClutched) {
+        Master.SetRobotControlState(mtsStdString("Clutch"));
+    } else {
+        if (IsOperatorPresent) {
+            Master.SetRobotControlState(mtsStdString("Gravity"));
+        } else {
+            MasterLockTranslation.Assign(Master.PositionCartesianCurrent.Position().Translation());
+            Master.SetRobotControlState(mtsStdString("DVRK_POSITION_CARTESIAN"));
+            Master.PositionCartesianDesired.SetGoal(Master.PositionCartesianCurrent.Position());
+            Master.SetPositionCartesian(Master.PositionCartesianDesired);
+        }
+    }
+
+    // Update MTM/PSM previous position
+    Master.CartesianPrevious.From(Master.PositionCartesianCurrent.Position());
+    Slave.CartesianPrevious.From(Slave.PositionCartesianCurrent.Position());
+}
+#endif
---
+++ include/sawIntuitiveResearchKit/mtsTeleOperationBimanual.h
@@ -0,0 +1,113 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+
+/*
+  Author(s):  Anton Deguet
+  Created on: 2016-01-21
+
+  (C) Copyright 2016 Johns Hopkins University (JHU), All Rights Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+*/
+
+#ifndef _mtsTeleOperationBimanual_h
+#define _mtsTeleOperationBimanual_h
+
+#include <cisstMultiTask/mtsTaskPeriodic.h>
+#include <cisstParameterTypes/prmEventButton.h>
+#include <cisstParameterTypes/prmPositionCartesianGet.h>
+#include <cisstParameterTypes/prmPositionCartesianSet.h>
+#include <cisstRobot/robManipulator.h>
+
+class mtsTeleOperationBimanual: public mtsTaskPeriodic
+{
+    CMN_DECLARE_SERVICES(CMN_DYNAMIC_CREATION_ONEARG, CMN_LOG_ALLOW_DEFAULT);
+
+public:
+    mtsTeleOperationBimanual(const std::string & componentName, const double periodInSeconds);
+    mtsTeleOperationBimanual(const mtsTaskPeriodicConstructorArg & arg);
+    ~mtsTeleOperationBimanual() {}
+
+    void Configure(const std::string & filename = "");
+    void Startup(void);
+    void Run(void);
+    void Cleanup(void);
+
+    void SetScale(const double & scale);
+    void SetRegistrationRotation(const vctMatRot3 & rotation);
+
+private:
+
+    void Init(void);
+
+    // Event Handler
+    void MasterLeftErrorEventHandler(const std::string & message);
+    void MasterRightErrorEventHandler(const std::string & message);
+    void SlaveErrorEventHandler(const std::string & message);
+
+    void SlaveClutchEventHandler(const prmEventButton & button);
+    void CameraClutchEventHandler(const prmEventButton & button);
+    void ClutchEventHandler(const prmEventButton & button);
+    void OperatorPresentEventHandler(const prmEventButton & button);
+
+    // Functions for events
+    struct {
+        mtsFunctionWrite Status;
+        mtsFunctionWrite Warning;
+        mtsFunctionWrite Error;
+        mtsFunctionWrite Enabled;
+    } MessageEvents;
+
+    struct {
+        mtsFunctionWrite Scale;
+    } ConfigurationEvents;
+
+    void Enable(const bool & enable);
+
+    void SetMasterControlState(void);
+
+protected:
+
+    class RobotMaster {
+    public:
+        mtsFunctionRead GetPositionCartesian;
+        mtsFunctionWrite SetRobotControlState;
+        prmPositionCartesianGet PositionCartesianCurrent;
+    };
+    RobotMaster mMasterLeft, mMasterRight;
+
+    class RobotSlave {
+    public:
+        mtsFunctionRead GetPositionCartesian;
+        mtsFunctionWrite SetPositionCartesian;
+        mtsFunctionWrite SetRobotControlState;
+
+        prmPositionCartesianGet PositionCartesianCurrent;
+        prmPositionCartesianSet PositionCartesianDesired;
+        vctFrm4x4 CartesianPrevious;
+        bool IsManipClutched;
+        bool IsSUJClutched;
+    };
+    RobotSlave mSlave;
+
+private:
+
+    double mScale;
+    vctMatRot3 mRegistrationRotation;
+
+    bool mIsClutched;
+    bool mIsOperatorPresent;
+    bool mIsEnabled;
+
+    mtsStateTable * mConfigurationStateTable;
+};
+
+CMN_DECLARE_SERVICES_INSTANTIATION(mtsTeleOperationBimanual);
+
+#endif // _mtsTeleOperationBimanual_h
