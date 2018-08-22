926f02e058d934e2996bf8a9c7d2f92e8acbd7ab
Anton Deguet <anton.deguet@jhu.edu>
2018-08-22 11:24:02 -0400

    robManipulatorRMRC: fixed inequality constraints for joint limits

--- components/code/robManipulatorRMRC.cpp
+++ components/code/robManipulatorRMRC.cpp
@@ -2,7 +2,7 @@
 /* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
 
 /*
-  Author(s):  Simon Leonard, Anton Deguet
+  Author(s):  Simon Leonard, Anton Deguet, Preetham Chalasani
   Created on: 2017-03-07
 
   (C) Copyright 2017-2018 Johns Hopkins University (JHU), All Rights Reserved.
@@ -37,85 +37,59 @@ robManipulatorRMRC::robManipulatorRMRC(const vctFrame4x4<double> & Rtw0)
 
 void robManipulatorRMRC::Resize(void)
 {
-    if (m.E.cols() == links.size()) {
-        return;
+    // use A to figure out if the LSEI memory is already allocated
+    if (m.A.cols() != links.size()) {
+        // Ex = f
+        m.E.SetSize(0, 0, VCT_COL_MAJOR);
+        m.f.SetSize(0, 0, VCT_COL_MAJOR);
+
+        // || Ax - B ||
+        m.A.SetSize(6, links.size(), VCT_COL_MAJOR);
+        m.A.SetAll(0.0);
+        m.b.SetSize(6, 1, VCT_COL_MAJOR);
+        m.b.SetAll(0.0);
+
+        // Gx >= h, the matrix is 2 nb links * nb links to have all
+        // the min and max joint limits
+        m.G.SetSize(2 * links.size(), links.size(), VCT_COL_MAJOR);
+        m.G.SetAll(0.0);
+        m.h.SetSize(2 * links.size(), 1, VCT_COL_MAJOR );
+        m.h.SetAll(0.0);
+
+        // activated inequality constraints, the "top" diagonal is all
+        // +1 for negative limits, i.e. dq will have to be positive to
+        // respect constaint, "bottom" diagonal is all -1 for positive
+        // limits.
+        for (size_t i = 0; i < links.size(); i++) {
+            m.G.Element(i, i) = 1.0;
+            m.G.Element(links.size() + i, i) = -1.0;
+        }
     }
-
-    // Ex = f
-    m.E.SetSize(0, 0, VCT_COL_MAJOR);
-    m.f.SetSize(0, 0, VCT_COL_MAJOR);
-
-    // || Ax - B ||
-    m.A.SetSize(6, links.size(), VCT_COL_MAJOR);
-    m.A.SetAll(0.0);
-    m.b.SetSize(6, 1, VCT_COL_MAJOR);
-    m.b.SetAll(0.0);
-#if 0
-    // Gx >= h
-    m.G.SetSize(2 * links.size(), links.size(), VCT_COL_MAJOR);
-    m.G.SetAll(0.0);
-    m.h.SetSize(2 * links.size(), 1, VCT_COL_MAJOR );
-    m.h.SetAll(0.0);
-
-    for (size_t i = 0; i < links.size(); i++) {
-        m.G.Element(i, i) = 1.0;
-        m.G.Element(links.size() + i, i) = -1.0;
-        // m.h.Element(i, 0) = links[i].GetKinematics()->PositionMin();
-        // m.h.Element(links.size() + i, 0) = -links[i].GetKinematics()->PositionMax();
-    }
-
+    // allocate should be called every time before calling Solve
     m.lsei.Allocate(m.E, m.A, m.G);
-#endif
 }
 
 vctReturnDynamicVector<double>
 robManipulatorRMRC::ConstrainedRMRC(const vctDynamicVector<double> & q,
                                     const vctFixedSizeVector<double, 6> & vw)
 {
+    // resize and reset, this needs to be called before modifying m
+    // members and before calling m.lsei
+    Resize();
+
     JacobianSpatial(q, m.A);
     m.b.Column(0) = vw;
 
-#if 1
-    // compute size of G
-    size_t sizeG = 0;
-    for (size_t i = 0; i < q.size(); i++ ) {
-        if (q[i] <= links[i].GetKinematics()->PositionMin()) {
-            sizeG++;
-        }
-        if( q[i] >= links[i].GetKinematics()->PositionMax()) {
-            sizeG++;
-        }
-    }
-    // Gx >= h
-    m.G.SetSize(sizeG, links.size(), VCT_COL_MAJOR);
-    m.G.SetAll(0.0);
-    m.h.SetSize(sizeG, 1, VCT_COL_MAJOR );
-    m.h.SetAll(0.0);
-
-    size_t indexG = 0;
-    for (size_t i = 0; i < links.size(); i++) {
-        if (q[i] <= links[i].GetKinematics()->PositionMin()) {
-            m.G[indexG][i] = 1.0;
-            m.h[indexG][0] = links[i].GetKinematics()->PositionMin();
-            indexG++;
-        }
-        if (q[i] >= links[i].GetKinematics()->PositionMax()) {
-            m.G[indexG][i] = -1.0;
-            m.h[indexG][0] = -links[i].GetKinematics()->PositionMax();
-            indexG++;
-        }
-    }
-
-    m.lsei.Allocate(m.E, m.A, m.G);
-
-#else
+    // h has to be recalculated to represent which new inequality
+    // constraints on dq based on q and q_limits
     for (size_t i = 0; i < links.size(); i++) {
         m.h.Element(i, 0) = links[i].GetKinematics()->PositionMin() - q[i];
         m.h.Element(links.size() + i, 0) = q[i] - links[i].GetKinematics()->PositionMax();
     }
 
-#endif
+    // solve for dq
     m.lsei.Solve(m.E, m.f, m.A, m.b, m.G, m.h);
+
     return vctReturnDynamicVector<double>(m.lsei.GetX().Column(0));
 }
 
@@ -139,8 +113,6 @@ robManipulatorRMRC::InverseKinematics(vctDynamicVector<double> & q,
         return robManipulator::EFAILURE;
     }
 
-    Resize();
-
     double ndq = 1.0;               // norm of the iteration error
     size_t i = 0;
     // loop until Niter are executed or the error is bellow the tolerance
@@ -153,7 +125,7 @@ robManipulatorRMRC::InverseKinematics(vctDynamicVector<double> & q,
         vctFixedSizeVector<double,3> dt(Rts[0][3] - Rt[0][3],
                                         Rts[1][3] - Rt[1][3],
                                         Rts[2][3] - Rt[2][3]);
-        
+
         // compute the orientation error
         vctFixedSizeVector<double,3> dr = 0.5 * ((Rt.Rotation().Column(0) % Rts.Rotation().Column(0)) +
                                                  (Rt.Rotation().Column(1) % Rts.Rotation().Column(1)) +
@@ -162,6 +134,7 @@ robManipulatorRMRC::InverseKinematics(vctDynamicVector<double> & q,
         // combine both errors in one R^6 vector
         vctFixedSizeVector<double,6> e(dt[0], dt[1], dt[2], dr[0], dr[1], dr[2]);
 
+        // get dq from optimizer with constaints
         vctDynamicVector<double> dq = ConstrainedRMRC(q, e);
 
         // compute the L2 norm of the error
