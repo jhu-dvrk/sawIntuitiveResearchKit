46b8a79b9b9436c474d6b6fccfea845b8049558e
Vincent Hui <vincenthk007@gmail.com>
2019-01-07 15:40:05 +0800

    reuse sin cos results when computing joint efforts for GC

--- components/code/robGravityCompensationMTM.cpp
+++ components/code/robGravityCompensationMTM.cpp
@@ -46,122 +46,133 @@ void robGravityCompensationMTM::AssignRegressor(const vctVec & q,
     const double q4 = q[3];
     const double q5 = q[4];
     const double q6 = q[5];
+    const double sq2 = sin(q2);
+    const double cq2 = cos(q2);
+    const double sq3 = sin(q3);
+    const double cq3 = cos(q3);
+    const double sq4 = sin(q4);
+    const double cq4 = cos(q4);
+    const double sq5 = sin(q5);
+    const double cq5 = cos(q5);
+    const double sq6 = sin(q6);
+    const double cq6 = cos(q6);
+    const double sq2pq3 = sin(q2 + q3);
 
     regressor[0][10] = 1.0;
     regressor[0][11] = q1;
     regressor[0][12] = std::pow(q1, 2);
     regressor[0][13] = std::pow(q1, 3);
     regressor[0][14] = std::pow(q1, 4);
-    regressor[1][0] = g * sin(q2);
-    regressor[1][1] = g * cos(q2);
-    regressor[1][2] = g * cos(q2) * cos(q3) - g * sin(q2) * sin(q3);
-    regressor[1][3] = -g * cos(q2) * sin(q3) - g * cos(q3) * sin(q2);
+    regressor[1][0] = g * sq2;
+    regressor[1][1] = g * cq2;
+    regressor[1][2] = g * cq2 * cq3 - g * sq2 *sq3;
+    regressor[1][3] = -g * cq2 *sq3 - g * cq3 * sq2;
     regressor[1][4] =
-        g * cos(q2) * cos(q3) * cos(q4) - g * cos(q4) * sin(q2) * sin(q3);
+        g * cq2 * cq3 * cq4 - g * cq4 * sq2 *sq3;
     regressor[1][5] =
-        g * sin(q2) * sin(q3) * sin(q4) - g * cos(q2) * cos(q3) * sin(q4);
-    regressor[1][6] = g * cos(q4) * sin(q2) * sin(q3) * sin(q5) -
-        g * cos(q3) * cos(q5) * sin(q2) -
-        g * cos(q2) * cos(q3) * cos(q4) * sin(q5) -
-        g * cos(q2) * cos(q5) * sin(q3);
-    regressor[1][7] = g * cos(q2) * cos(q3) * cos(q4) * cos(q5) -
-        g * cos(q3) * sin(q2) * sin(q5) -
-        g * cos(q2) * sin(q3) * sin(q5) -
-        g * cos(q4) * cos(q5) * sin(q2) * sin(q3);
-    regressor[1][8] = g * cos(q2) * cos(q3) * sin(q4) * sin(q6) +
-        g * cos(q2) * cos(q6) * sin(q3) * sin(q5) +
-        g * cos(q3) * cos(q6) * sin(q2) * sin(q5) -
-        g * sin(q2) * sin(q3) * sin(q4) * sin(q6) +
-        g * cos(q4) * cos(q5) * cos(q6) * sin(q2) * sin(q3) -
-        g * cos(q2) * cos(q3) * cos(q4) * cos(q5) * cos(q6);
-    regressor[1][9] = g * cos(q2) * cos(q3) * cos(q6) * sin(q4) -
-        g * cos(q6) * sin(q2) * sin(q3) * sin(q4) -
-        g * cos(q2) * sin(q3) * sin(q5) * sin(q6) -
-        g * cos(q3) * sin(q2) * sin(q5) * sin(q6) -
-        g * cos(q4) * cos(q5) * sin(q2) * sin(q3) * sin(q6) +
-        g * cos(q2) * cos(q3) * cos(q4) * cos(q5) * sin(q6);
+        g * sq2 *sq3 *sq4 - g * cq2 * cq3 *sq4;
+    regressor[1][6] = g * cq4 * sq2 *sq3 *sq5 -
+        g * cq3 * cq5 * sq2 -
+        g * cq2 * cq3 * cq4 *sq5 -
+        g * cq2 * cq5 *sq3;
+    regressor[1][7] = g * cq2 * cq3 * cq4 * cq5 -
+        g * cq3 * sq2 *sq5 -
+        g * cq2 *sq3 *sq5 -
+        g * cq4 * cq5 * sq2 *sq3;
+    regressor[1][8] = g * cq2 * cq3 *sq4 *sq6 +
+        g * cq2 * cq6 *sq3 *sq5 +
+        g * cq3 * cq6 * sq2 *sq5 -
+        g * sq2 *sq3 *sq4 *sq6 +
+        g * cq4 * cq5 * cq6 * sq2 *sq3 -
+        g * cq2 * cq3 * cq4 * cq5 * cq6;
+    regressor[1][9] = g * cq2 * cq3 * cq6 *sq4 -
+        g * cq6 * sq2 *sq3 *sq4 -
+        g * cq2 *sq3 *sq5 *sq6 -
+        g * cq3 * sq2 *sq5 *sq6 -
+        g * cq4 * cq5 * sq2 *sq3 *sq6 +
+        g * cq2 * cq3 * cq4 * cq5 *sq6;
     regressor[1][15] = 1.0;
     regressor[1][16] = q2;
     regressor[1][17] = std::pow(q2, 2);
     regressor[1][18] = std::pow(q2, 3);
     regressor[1][19] = std::pow(q2, 4);
-    regressor[2][2] = g * cos(q2) * cos(q3) - g * sin(q2) * sin(q3);
-    regressor[2][3] = -g * cos(q2) * sin(q3) - g * cos(q3) * sin(q2);
+    regressor[2][2] = g * cq2 * cq3 - g * sq2 *sq3;
+    regressor[2][3] = -g * cq2 *sq3 - g * cq3 * sq2;
     regressor[2][4] =
-        g * cos(q2) * cos(q3) * cos(q4) - g * cos(q4) * sin(q2) * sin(q3);
+        g * cq2 * cq3 * cq4 - g * cq4 * sq2 *sq3;
     regressor[2][5] =
-        g * sin(q2) * sin(q3) * sin(q4) - g * cos(q2) * cos(q3) * sin(q4);
-    regressor[2][6] = g * cos(q4) * sin(q2) * sin(q3) * sin(q5) -
-        g * cos(q3) * cos(q5) * sin(q2) -
-        g * cos(q2) * cos(q3) * cos(q4) * sin(q5) -
-        g * cos(q2) * cos(q5) * sin(q3);
-    regressor[2][7] = g * cos(q2) * cos(q3) * cos(q4) * cos(q5) -
-        g * cos(q3) * sin(q2) * sin(q5) -
-        g * cos(q2) * sin(q3) * sin(q5) -
-        g * cos(q4) * cos(q5) * sin(q2) * sin(q3);
-    regressor[2][8] = g * cos(q2) * cos(q3) * sin(q4) * sin(q6) +
-        g * cos(q2) * cos(q6) * sin(q3) * sin(q5) +
-        g * cos(q3) * cos(q6) * sin(q2) * sin(q5) -
-        g * sin(q2) * sin(q3) * sin(q4) * sin(q6) +
-        g * cos(q4) * cos(q5) * cos(q6) * sin(q2) * sin(q3) -
-        g * cos(q2) * cos(q3) * cos(q4) * cos(q5) * cos(q6);
-    regressor[2][9] = g * cos(q2) * cos(q3) * cos(q6) * sin(q4) -
-        g * cos(q6) * sin(q2) * sin(q3) * sin(q4) -
-        g * cos(q2) * sin(q3) * sin(q5) * sin(q6) -
-        g * cos(q3) * sin(q2) * sin(q5) * sin(q6) -
-        g * cos(q4) * cos(q5) * sin(q2) * sin(q3) * sin(q6) +
-        g * cos(q2) * cos(q3) * cos(q4) * cos(q5) * sin(q6);
+        g * sq2 *sq3 *sq4 - g * cq2 * cq3 *sq4;
+    regressor[2][6] = g * cq4 * sq2 *sq3 *sq5 -
+        g * cq3 * cq5 * sq2 -
+        g * cq2 * cq3 * cq4 *sq5 -
+        g * cq2 * cq5 *sq3;
+    regressor[2][7] = g * cq2 * cq3 * cq4 * cq5 -
+        g * cq3 * sq2 *sq5 -
+        g * cq2 *sq3 *sq5 -
+        g * cq4 * cq5 * sq2 *sq3;
+    regressor[2][8] = g * cq2 * cq3 *sq4 *sq6 +
+        g * cq2 * cq6 *sq3 *sq5 +
+        g * cq3 * cq6 * sq2 *sq5 -
+        g * sq2 *sq3 *sq4 *sq6 +
+        g * cq4 * cq5 * cq6 * sq2 *sq3 -
+        g * cq2 * cq3 * cq4 * cq5 * cq6;
+    regressor[2][9] = g * cq2 * cq3 * cq6 *sq4 -
+        g * cq6 * sq2 *sq3 *sq4 -
+        g * cq2 *sq3 *sq5 *sq6 -
+        g * cq3 * sq2 *sq5 *sq6 -
+        g * cq4 * cq5 * sq2 *sq3 *sq6 +
+        g * cq2 * cq3 * cq4 * cq5 *sq6;
     regressor[2][20] = 1.0;
     regressor[2][21] = q3;
     regressor[2][22] = std::pow(q3, 2);
     regressor[2][23] = std::pow(q3, 3);
     regressor[2][24] = std::pow(q3, 4);
-    regressor[3][4] = -g * sin(q2 + q3) * sin(q4);
-    regressor[3][5] = -g * sin(q2 + q3) * cos(q4);
-    regressor[3][6] = g * sin(q2 + q3) * sin(q4) * sin(q5);
-    regressor[3][7] = -g * sin(q2 + q3) * cos(q5) * sin(q4);
+    regressor[3][4] = -g * sq2pq3 *sq4;
+    regressor[3][5] = -g * sq2pq3 * cq4;
+    regressor[3][6] = g * sq2pq3 *sq4 *sq5;
+    regressor[3][7] = -g * sq2pq3 * cq5 *sq4;
     regressor[3][8] =
-        g * sin(q2 + q3) * (cos(q4) * sin(q6) + cos(q5) * cos(q6) * sin(q4));
+        g * sq2pq3 * (cq4 *sq6 + cq5 * cq6 *sq4);
     regressor[3][9] =
-        g * sin(q2 + q3) * (cos(q4) * cos(q6) - cos(q5) * sin(q4) * sin(q6));
+        g * sq2pq3 * (cq4 * cq6 - cq5 *sq4 *sq6);
     regressor[3][25] = 1.0;
     regressor[3][26] = q4;
     regressor[3][27] = std::pow(q4, 2);
     regressor[3][28] = std::pow(q4, 3);
     regressor[3][29] = std::pow(q4, 4);
     regressor[4][6] =
-        -g * (cos(q2) * cos(q3) * sin(q5) - sin(q2) * sin(q3) * sin(q5) +
-              cos(q2) * cos(q4) * cos(q5) * sin(q3) +
-              cos(q3) * cos(q4) * cos(q5) * sin(q2));
+        -g * (cq2 * cq3 *sq5 - sq2 *sq3 *sq5 +
+              cq2 * cq4 * cq5 *sq3 +
+              cq3 * cq4 * cq5 * sq2);
     regressor[4][7] =
-        -g * (cos(q5) * sin(q2) * sin(q3) - cos(q2) * cos(q3) * cos(q5) +
-              cos(q2) * cos(q4) * sin(q3) * sin(q5) +
-              cos(q3) * cos(q4) * sin(q2) * sin(q5));
-    regressor[4][8] = g * (cos(q5) * cos(q6) * sin(q2) * sin(q3) -
-                           cos(q2) * cos(q3) * cos(q5) * cos(q6) +
-                           cos(q2) * cos(q4) * cos(q6) * sin(q3) * sin(q5) +
-                           cos(q3) * cos(q4) * cos(q6) * sin(q2) * sin(q5));
-    regressor[4][9] = -g * (cos(q5) * sin(q2) * sin(q3) * sin(q6) -
-                            cos(q2) * cos(q3) * cos(q5) * sin(q6) +
-                            cos(q2) * cos(q4) * sin(q3) * sin(q5) * sin(q6) +
-                            cos(q3) * cos(q4) * sin(q2) * sin(q5) * sin(q6));
+        -g * (cq5 * sq2 *sq3 - cq2 * cq3 * cq5 +
+              cq2 * cq4 *sq3 *sq5 +
+              cq3 * cq4 * sq2 *sq5);
+    regressor[4][8] = g * (cq5 * cq6 * sq2 *sq3 -
+                           cq2 * cq3 * cq5 * cq6 +
+                           cq2 * cq4 * cq6 *sq3 *sq5 +
+                           cq3 * cq4 * cq6 * sq2 *sq5);
+    regressor[4][9] = -g * (cq5 * sq2 *sq3 *sq6 -
+                            cq2 * cq3 * cq5 *sq6 +
+                            cq2 * cq4 *sq3 *sq5 *sq6 +
+                            cq3 * cq4 * sq2 *sq5 *sq6);
     regressor[4][30] = 1.0;
     regressor[4][31] = q5;
     regressor[4][32] = std::pow(q5, 2);
     regressor[4][33] = std::pow(q5, 3);
     regressor[4][34] = std::pow(q5, 4);
-    regressor[5][8] = g * (cos(q2) * cos(q6) * sin(q3) * sin(q4) +
-                           cos(q3) * cos(q6) * sin(q2) * sin(q4) +
-                           cos(q2) * cos(q3) * sin(q5) * sin(q6) -
-                           sin(q2) * sin(q3) * sin(q5) * sin(q6) +
-                           cos(q2) * cos(q4) * cos(q5) * sin(q3) * sin(q6) +
-                           cos(q3) * cos(q4) * cos(q5) * sin(q2) * sin(q6));
-    regressor[5][9] = g * (cos(q2) * cos(q3) * cos(q6) * sin(q5) -
-                           cos(q2) * sin(q3) * sin(q4) * sin(q6) -
-                           cos(q3) * sin(q2) * sin(q4) * sin(q6) -
-                           cos(q6) * sin(q2) * sin(q3) * sin(q5) +
-                           cos(q2) * cos(q4) * cos(q5) * cos(q6) * sin(q3) +
-                           cos(q3) * cos(q4) * cos(q5) * cos(q6) * sin(q2));
+    regressor[5][8] = g * (cq2 * cq6 *sq3 *sq4 +
+                           cq3 * cq6 * sq2 *sq4 +
+                           cq2 * cq3 *sq5 *sq6 -
+                           sq2 *sq3 *sq5 *sq6 +
+                           cq2 * cq4 * cq5 *sq3 *sq6 +
+                           cq3 * cq4 * cq5 * sq2 *sq6);
+    regressor[5][9] = g * (cq2 * cq3 * cq6 *sq5 -
+                           cq2 *sq3 *sq4 *sq6 -
+                           cq3 * sq2 *sq4 *sq6 -
+                           cq6 * sq2 *sq3 *sq5 +
+                           cq2 * cq4 * cq5 * cq6 *sq3 +
+                           cq3 * cq4 * cq5 * cq6 * sq2);
     regressor[5][35] = 1.0;
     regressor[5][36] = q6;
     regressor[5][37] = std::pow(q6, 2);
