7288fffaaf7bef3c4209765fdb80c4c8e0de8674
Vincent Hui <vincenthk007@gmail.com>
2019-01-07 15:43:38 +0800

    use Element member function to access elements in the regressor matrix

--- components/code/robGravityCompensationMTM.cpp
+++ components/code/robGravityCompensationMTM.cpp
@@ -58,126 +58,126 @@ void robGravityCompensationMTM::AssignRegressor(const vctVec & q,
     const double cq6 = cos(q6);
     const double sq2pq3 = sin(q2 + q3);
 
-    regressor[0][10] = 1.0;
-    regressor[0][11] = q1;
-    regressor[0][12] = std::pow(q1, 2);
-    regressor[0][13] = std::pow(q1, 3);
-    regressor[0][14] = std::pow(q1, 4);
-    regressor[1][0] = g * sq2;
-    regressor[1][1] = g * cq2;
-    regressor[1][2] = g * cq2 * cq3 - g * sq2 *sq3;
-    regressor[1][3] = -g * cq2 *sq3 - g * cq3 * sq2;
-    regressor[1][4] =
+    regressor.Element(0,10) = 1.0;
+    regressor.Element(0,11) = q1;
+    regressor.Element(0,12) = std::pow(q1, 2);
+    regressor.Element(0,13) = std::pow(q1, 3);
+    regressor.Element(0,14) = std::pow(q1, 4);
+    regressor.Element(1,0) = g * sq2;
+    regressor.Element(1,1) = g * cq2;
+    regressor.Element(1,2) = g * cq2 * cq3 - g * sq2 *sq3;
+    regressor.Element(1,3) = -g * cq2 *sq3 - g * cq3 * sq2;
+    regressor.Element(1,4) =
         g * cq2 * cq3 * cq4 - g * cq4 * sq2 *sq3;
-    regressor[1][5] =
+    regressor.Element(1,5) =
         g * sq2 *sq3 *sq4 - g * cq2 * cq3 *sq4;
-    regressor[1][6] = g * cq4 * sq2 *sq3 *sq5 -
+    regressor.Element(1,6) = g * cq4 * sq2 *sq3 *sq5 -
         g * cq3 * cq5 * sq2 -
         g * cq2 * cq3 * cq4 *sq5 -
         g * cq2 * cq5 *sq3;
-    regressor[1][7] = g * cq2 * cq3 * cq4 * cq5 -
+    regressor.Element(1,7) = g * cq2 * cq3 * cq4 * cq5 -
         g * cq3 * sq2 *sq5 -
         g * cq2 *sq3 *sq5 -
         g * cq4 * cq5 * sq2 *sq3;
-    regressor[1][8] = g * cq2 * cq3 *sq4 *sq6 +
+    regressor.Element(1,8) = g * cq2 * cq3 *sq4 *sq6 +
         g * cq2 * cq6 *sq3 *sq5 +
         g * cq3 * cq6 * sq2 *sq5 -
         g * sq2 *sq3 *sq4 *sq6 +
         g * cq4 * cq5 * cq6 * sq2 *sq3 -
         g * cq2 * cq3 * cq4 * cq5 * cq6;
-    regressor[1][9] = g * cq2 * cq3 * cq6 *sq4 -
+    regressor.Element(1,9) = g * cq2 * cq3 * cq6 *sq4 -
         g * cq6 * sq2 *sq3 *sq4 -
         g * cq2 *sq3 *sq5 *sq6 -
         g * cq3 * sq2 *sq5 *sq6 -
         g * cq4 * cq5 * sq2 *sq3 *sq6 +
         g * cq2 * cq3 * cq4 * cq5 *sq6;
-    regressor[1][15] = 1.0;
-    regressor[1][16] = q2;
-    regressor[1][17] = std::pow(q2, 2);
-    regressor[1][18] = std::pow(q2, 3);
-    regressor[1][19] = std::pow(q2, 4);
-    regressor[2][2] = g * cq2 * cq3 - g * sq2 *sq3;
-    regressor[2][3] = -g * cq2 *sq3 - g * cq3 * sq2;
-    regressor[2][4] =
+    regressor.Element(1,15) = 1.0;
+    regressor.Element(1,16) = q2;
+    regressor.Element(1,17) = std::pow(q2, 2);
+    regressor.Element(1,18) = std::pow(q2, 3);
+    regressor.Element(1,19) = std::pow(q2, 4);
+    regressor.Element(2,2) = g * cq2 * cq3 - g * sq2 *sq3;
+    regressor.Element(2,3) = -g * cq2 *sq3 - g * cq3 * sq2;
+    regressor.Element(2,4) =
         g * cq2 * cq3 * cq4 - g * cq4 * sq2 *sq3;
-    regressor[2][5] =
+    regressor.Element(2,5) =
         g * sq2 *sq3 *sq4 - g * cq2 * cq3 *sq4;
-    regressor[2][6] = g * cq4 * sq2 *sq3 *sq5 -
+    regressor.Element(2,6) = g * cq4 * sq2 *sq3 *sq5 -
         g * cq3 * cq5 * sq2 -
         g * cq2 * cq3 * cq4 *sq5 -
         g * cq2 * cq5 *sq3;
-    regressor[2][7] = g * cq2 * cq3 * cq4 * cq5 -
+    regressor.Element(2,7) = g * cq2 * cq3 * cq4 * cq5 -
         g * cq3 * sq2 *sq5 -
         g * cq2 *sq3 *sq5 -
         g * cq4 * cq5 * sq2 *sq3;
-    regressor[2][8] = g * cq2 * cq3 *sq4 *sq6 +
+    regressor.Element(2,8) = g * cq2 * cq3 *sq4 *sq6 +
         g * cq2 * cq6 *sq3 *sq5 +
         g * cq3 * cq6 * sq2 *sq5 -
         g * sq2 *sq3 *sq4 *sq6 +
         g * cq4 * cq5 * cq6 * sq2 *sq3 -
         g * cq2 * cq3 * cq4 * cq5 * cq6;
-    regressor[2][9] = g * cq2 * cq3 * cq6 *sq4 -
+    regressor.Element(2,9) = g * cq2 * cq3 * cq6 *sq4 -
         g * cq6 * sq2 *sq3 *sq4 -
         g * cq2 *sq3 *sq5 *sq6 -
         g * cq3 * sq2 *sq5 *sq6 -
         g * cq4 * cq5 * sq2 *sq3 *sq6 +
         g * cq2 * cq3 * cq4 * cq5 *sq6;
-    regressor[2][20] = 1.0;
-    regressor[2][21] = q3;
-    regressor[2][22] = std::pow(q3, 2);
-    regressor[2][23] = std::pow(q3, 3);
-    regressor[2][24] = std::pow(q3, 4);
-    regressor[3][4] = -g * sq2pq3 *sq4;
-    regressor[3][5] = -g * sq2pq3 * cq4;
-    regressor[3][6] = g * sq2pq3 *sq4 *sq5;
-    regressor[3][7] = -g * sq2pq3 * cq5 *sq4;
-    regressor[3][8] =
+    regressor.Element(2,20) = 1.0;
+    regressor.Element(2,21) = q3;
+    regressor.Element(2,22) = std::pow(q3, 2);
+    regressor.Element(2,23) = std::pow(q3, 3);
+    regressor.Element(2,24) = std::pow(q3, 4);
+    regressor.Element(3,4) = -g * sq2pq3 *sq4;
+    regressor.Element(3,5) = -g * sq2pq3 * cq4;
+    regressor.Element(3,6) = g * sq2pq3 *sq4 *sq5;
+    regressor.Element(3,7) = -g * sq2pq3 * cq5 *sq4;
+    regressor.Element(3,8) =
         g * sq2pq3 * (cq4 *sq6 + cq5 * cq6 *sq4);
-    regressor[3][9] =
+    regressor.Element(3,9) =
         g * sq2pq3 * (cq4 * cq6 - cq5 *sq4 *sq6);
-    regressor[3][25] = 1.0;
-    regressor[3][26] = q4;
-    regressor[3][27] = std::pow(q4, 2);
-    regressor[3][28] = std::pow(q4, 3);
-    regressor[3][29] = std::pow(q4, 4);
-    regressor[4][6] =
+    regressor.Element(3,25) = 1.0;
+    regressor.Element(3,26) = q4;
+    regressor.Element(3,27) = std::pow(q4, 2);
+    regressor.Element(3,28) = std::pow(q4, 3);
+    regressor.Element(3,29) = std::pow(q4, 4);
+    regressor.Element(4,6) =
         -g * (cq2 * cq3 *sq5 - sq2 *sq3 *sq5 +
               cq2 * cq4 * cq5 *sq3 +
               cq3 * cq4 * cq5 * sq2);
-    regressor[4][7] =
+    regressor.Element(4,7) =
         -g * (cq5 * sq2 *sq3 - cq2 * cq3 * cq5 +
               cq2 * cq4 *sq3 *sq5 +
               cq3 * cq4 * sq2 *sq5);
-    regressor[4][8] = g * (cq5 * cq6 * sq2 *sq3 -
+    regressor.Element(4,8) = g * (cq5 * cq6 * sq2 *sq3 -
                            cq2 * cq3 * cq5 * cq6 +
                            cq2 * cq4 * cq6 *sq3 *sq5 +
                            cq3 * cq4 * cq6 * sq2 *sq5);
-    regressor[4][9] = -g * (cq5 * sq2 *sq3 *sq6 -
+    regressor.Element(4,9) = -g * (cq5 * sq2 *sq3 *sq6 -
                             cq2 * cq3 * cq5 *sq6 +
                             cq2 * cq4 *sq3 *sq5 *sq6 +
                             cq3 * cq4 * sq2 *sq5 *sq6);
-    regressor[4][30] = 1.0;
-    regressor[4][31] = q5;
-    regressor[4][32] = std::pow(q5, 2);
-    regressor[4][33] = std::pow(q5, 3);
-    regressor[4][34] = std::pow(q5, 4);
-    regressor[5][8] = g * (cq2 * cq6 *sq3 *sq4 +
+    regressor.Element(4,30) = 1.0;
+    regressor.Element(4,31) = q5;
+    regressor.Element(4,32) = std::pow(q5, 2);
+    regressor.Element(4,33) = std::pow(q5, 3);
+    regressor.Element(4,34) = std::pow(q5, 4);
+    regressor.Element(5,8) = g * (cq2 * cq6 *sq3 *sq4 +
                            cq3 * cq6 * sq2 *sq4 +
                            cq2 * cq3 *sq5 *sq6 -
                            sq2 *sq3 *sq5 *sq6 +
                            cq2 * cq4 * cq5 *sq3 *sq6 +
                            cq3 * cq4 * cq5 * sq2 *sq6);
-    regressor[5][9] = g * (cq2 * cq3 * cq6 *sq5 -
+    regressor.Element(5,9) = g * (cq2 * cq3 * cq6 *sq5 -
                            cq2 *sq3 *sq4 *sq6 -
                            cq3 * sq2 *sq4 *sq6 -
                            cq6 * sq2 *sq3 *sq5 +
                            cq2 * cq4 * cq5 * cq6 *sq3 +
                            cq3 * cq4 * cq5 * cq6 * sq2);
-    regressor[5][35] = 1.0;
-    regressor[5][36] = q6;
-    regressor[5][37] = std::pow(q6, 2);
-    regressor[5][38] = std::pow(q6, 3);
-    regressor[5][39] = std::pow(q6, 4);
+    regressor.Element(5,35) = 1.0;
+    regressor.Element(5,36) = q6;
+    regressor.Element(5,37) = std::pow(q6, 2);
+    regressor.Element(5,38) = std::pow(q6, 3);
+    regressor.Element(5,39) = std::pow(q6, 4);
 }
 
 void robGravityCompensationMTM::AddGravityCompensationEfforts(const vctVec & q,
