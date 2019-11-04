c15bb7c162a8e6320b8ca3b75102637823068fe2
Anton Deguet <anton.deguet@jhu.edu>
2019-11-04 16:12:35 -0500

    Changed default periods for IO and Arm to something IO is more likely to achieve reliably

--- components/include/sawIntuitiveResearchKit/mtsIntuitiveResearchKit.h
+++ components/include/sawIntuitiveResearchKit/mtsIntuitiveResearchKit.h
@@ -23,9 +23,10 @@ http://www.cisst.org/cisst/license.txt.
 #include <sawIntuitiveResearchKit/sawIntuitiveResearchKitExport.h>
 
 namespace mtsIntuitiveResearchKit {
-    const double IOPeriod = 0.3 * cmn_ms;
-    const double ArmPeriod = 0.5 * cmn_ms;
-    const double TeleopPeriod = 1.0 * cmn_ms;
+    const double PeriodDelay = 0.06 * cmn_ms; // fixed delay
+    const double IOPeriod = cmnHzToPeriod(1500.0) - PeriodDelay;
+    const double ArmPeriod = cmnHzToPeriod(1500.0) - PeriodDelay;
+    const double TeleopPeriod = cmnHzToPeriod(1000.0) - PeriodDelay;
     const double WatchdogTimeout = 30.0 * cmn_ms;
 
     // DO NOT INCREASE THIS ABOVE 3 SECONDS!!!  Some power supplies
