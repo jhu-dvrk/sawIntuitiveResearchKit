631e79968679c2fbd5c33c0d611359c60bfd0ee2
Anton Deguet <anton.deguet@jhu.edu>
2021-03-31 15:11:12 -0400

    schemas: $ref somewhat working

--- share/schemas/generate-html.sh
+++ share/schemas/generate-html.sh
@@ -2,5 +2,10 @@
 
 # quick hack, this should be a Makefile or proper script with option to set output directory
 
+mkdir -p v2.0
+
 generate-schema-doc --deprecated-from-description dvrk-console.schema.json v2.0/dvrk-console.html
 generate-schema-doc cisst-component-manager.schema.json --deprecated-from-description v2.0/cisst-component-manager.html
+
+cp  dvrk-console.schema.json v2.0
+cp  cisst-component-manager.schema.json v2.0
--- share/schemas/json-schema.py
+++ share/schemas/json-schema.py
@@ -1,10 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # simple program to load a JSON schema and file to validate.  this is
 # a bit better than the jsonschema program provided with jsonschema
 # since it will first check the JSON syntax of both files.  we also
 # use jsmin to remove comments from configuration file.
 
+# resolving references should be simple but I can't figure it out.  So
+# I implemented something hackish based on
+# https://stackoverflow.com/questions/42159346/jsonschema-refresolver-to-resolve-multiple-refs-in-python
+
+import os
 import sys
 import argparse
 import jsmin
@@ -29,6 +34,28 @@ with open(args.file) as f:
 with open(args.schema) as s:
   schema = json.load(s)
 
-jsonschema.validate(instance = file, schema = schema)
+# the following is to be able to locate all possible $ref in schema -- this seems very hackish
+schemastore = {}
+
+# get the directory for the schema
+tmp = open(args.schema)
+current_schema_directory = os.path.dirname(os.path.realpath(tmp.name))
+print("Using schemas from: {}".format(current_schema_directory))
+
+# find all files in directory
+all_files = os.listdir(current_schema_directory)
+for that_file in all_files:
+  that_file_full_path = os.path.join(current_schema_directory, that_file)
+  extension = ".schema.json"
+  if that_file_full_path[-len(extension):] == extension:
+    print("Found possible schema file: {}".format(that_file))
+    with open(that_file_full_path, "r") as schema_file_descriptor:
+      that_schema = json.load(schema_file_descriptor)
+      if "id" in that_schema:
+        schemastore[that_schema["id"]] = that_schema
+
+resolver = jsonschema.RefResolver(args.schema, schema, schemastore)
+validator = jsonschema.Draft7Validator(schema, resolver = resolver)
+validator.validate(file)
 
 print ('All good')
