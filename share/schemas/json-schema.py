8261f87df7f2a093dedfa6cc1f8249f66a0cb9b7
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2025-02-26 19:00:04 -0500

    Update schema reference resolving
    Replace deprecated RefResolver with referencing library

--- share/schemas/json-schema.py
+++ share/schemas/json-schema.py
@@ -5,16 +5,14 @@
 # since it will first check the JSON syntax of both files.  we also
 # use jsmin to remove comments from configuration file.
 
-# resolving references should be simple but I can't figure it out.  So
-# I implemented something hackish based on
-# https://stackoverflow.com/questions/42159346/jsonschema-refresolver-to-resolve-multiple-refs-in-python
-
 import os
 import sys
 import argparse
 import jsmin
 import json
 import jsonschema
+import referencing
+import pathlib
 
 # parse arguments
 parser = argparse.ArgumentParser(description = 'json-schema')
@@ -27,35 +25,24 @@ parser.add_argument('files', metavar = 'N', type = str, nargs = '+',
 
 args = parser.parse_args(sys.argv[1:]) # skip argv[0], script name
 
+schema_path = pathlib.Path(args.schema).resolve()
+with open(schema_path) as s:
+  schema = json.load(s)
+
+schema_dir = schema_path.parent
+print('Using schemas from: {}'.format(schema_dir))
+
+def retrieve_callback(filename: str):
+  path = schema_dir / filename
+  contents = json.loads(path.read_text())
+  return referencing.Resource.from_contents(contents)
+
+registry = referencing.Registry(retrieve=retrieve_callback)
+validator = jsonschema.Draft7Validator(schema, registry=registry)
+
 # we let the following calls to "load" and "validate" throw
 # exceptions, users have to read the trace back to understand the
 # issues
-with open(args.schema) as s:
-  schema = json.load(s)
-
-# get the directory for the schema
-tmp = open(args.schema)
-current_schema_directory = os.path.dirname(os.path.realpath(tmp.name))
-print('Using schemas from: {}'.format(current_schema_directory))
-
-# the following is to be able to locate all possible $ref in schema
-schema_store = {}
-
-# find all files in directory
-all_files = os.listdir(current_schema_directory)
-for that_file in all_files:
-  that_file_full_path = os.path.join(current_schema_directory, that_file)
-  extension = '.schema.json'
-  if that_file_full_path[-len(extension):] == extension:
-    print('Found possible schema file: {}'.format(that_file))
-    with open(that_file_full_path, 'r') as schema_file_descriptor:
-      that_schema = json.load(schema_file_descriptor)
-      if '$id' in that_schema:
-        print(' - $id: {}'.format(that_schema['$id']))
-        schema_store[that_schema['$id']] = that_schema
-
-resolver = jsonschema.RefResolver.from_schema(schema, store = schema_store)
-validator = jsonschema.Draft7Validator(schema, resolver = resolver)
 
 # finally loop over files provided
 for json_file in args.files:
